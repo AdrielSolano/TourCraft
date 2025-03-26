4a9cfe96e67377d81ec22e18ae93ec31cefe1e89
AdrielSolano <2302019@utrivieramaya.edu.mx>
2025-03-25 22:06:16 -0500

    hgfd

--- src/Components/Guides.jsx
+++ src/Components/Guides.jsx
@@ -15,6 +15,7 @@ import {
     Typography,
 } from "@mui/material";
 import React from "react";
+import { Link as RouterLink } from 'react-router-dom';
 
 // Sample data for guides
 const guides = [
