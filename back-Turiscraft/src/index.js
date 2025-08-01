376651f8ce5cc548ac66d914409b44b9cf190bc8
AdrielSolano <2302019@utrivieramaya.edu.mx>
2025-08-01 02:42:40 -0500

    sss

--- back-Turiscraft/src/index.js
+++ back-Turiscraft/src/index.js
@@ -6,11 +6,4 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}`); 
 })
 
-const path = require("path");
 
-// Servir React desde Express (después de tus rutas API)
-app.use(express.static(path.join(__dirname, "..", "client", "build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
-});
